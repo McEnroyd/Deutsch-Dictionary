6d40a2a1d0680be6e0988f91a051d698f5792e5a
McEnroyd <iraklinaneishvili13@gmail.com>
2024-07-03 21:46:27 +0400

    Initial Commit

---
+++ function.py
@@ -0,0 +1,116 @@
+import json
+import random
+
+
+def add_new_word(option):
+    word = {}
+    if option == "n":
+        in_german = input("Enter noun in German: ")
+        in_german_plural = input("Enter noun in plural: ")
+        in_georgian = input("Enter noun in Georgian: ")
+        word = {
+            "form": option,
+            "in_german": in_german,
+            "in_german_plural": in_german_plural,
+            "in_georgian": in_georgian
+        }
+
+    elif option == "v":
+        in_german = input("Enter verb in German: ")
+        in_georgian = input("Enter verb in Georgian: ")
+        word = {
+            "form": option,
+            "in_german": in_german,
+            "in_georgian": in_georgian
+        }
+
+    elif option == "sv":
+        infinitiv = input("Infinitiv: ")
+        imperfekt = input("Imperfekt: ")
+        partizip_2 = input("Partizip II: ")
+        hilfsverb = input("Hilfsverb: ")
+        in_georgian = input("In Georgian: ")
+
+        word = {
+            "form": option,
+            "infinitiv": infinitiv,
+            "imperfekt": imperfekt,
+            "partizip_2": partizip_2,
+            "hilfsverb": hilfsverb,
+            "in_georgian": in_georgian
+        }
+
+    elif option == "o":
+        sentence = input("Enter sentence in German: ")
+        in_georgian = input("Enter in Georgian: ")
+        word = {
+            "form": option,
+            "in_german": sentence,
+            "in_georgian": in_georgian
+        }
+
+    try:
+        lexicon = get_lex("standart_words.json" if option in ["n", "v", "o"] else "sv_dictionary.json")
+        lexicon.append(word)
+        with open("standart_words.json" if option in ["n", "v", "o"] else "sv_dictionary.json", "w") as file:
+            json.dump(lexicon, file)
+        ind_numb = len(lexicon)
+
+        if word["form"] == "n":
+            print(f"Added: {ind_numb}.{word['in_german']} - {word['in_german_plural']} - {word['in_georgian']}")
+        elif word["form"] == "v":
+            print(f"Added: {ind_numb}.{word['in_german']} - {word['in_georgian']}")
+        elif word["form"] == "o":
+            print(f"Added: {ind_numb}.{word['in_german']} - {word['in_georgian']}")
+        elif word["form"] == "sv":
+            print(
+                f"Added: {ind_numb}.{word['infinitiv']} - {word['imperfekt']} - {word['partizip_2']} - {word['hilfsverb']} - {word['in_georgian']}")
+    except KeyError:
+        print("Unknown command")
+
+
+def del_word(opt):
+    try:
+        ind1 = int(input("Enter number to delete: ")) - 1
+        if ind1 < 0:
+            print("Enter more than 0")
+            return
+        if opt == "sd":
+            lexicon = get_lex("standart_words.json")
+            deleted = lexicon.pop(ind1)
+            print(len(lexicon))
+            with open("standart_words.json", "w") as file:
+                json.dump(lexicon, file)
+                print(f"Deleted - {deleted['in_german']}")
+        elif opt == "sv":
+            lexicon = get_lex("sv_dictionary.json")
+            deleted = lexicon.pop(ind1)
+            with open("sv_dictionary.json", "w") as file:
+                json.dump(lexicon, file)
+            print(f"Deleted {deleted['infinitiv']}")
+    except ValueError:
+        print("Enter only numbers")
+    except IndexError:
+        print("Number ot of range")
+
+
+def quiz_print(w, random_index):
+    user_answeringerman = input(f"{w['in_georgian']}: ")
+    if w["in_german"] != user_answeringerman:
+        print(f"Incorrect, your answer: {user_answeringerman} / "
+              f"correct: {random_index + 1}.{w['in_german']}")
+    else:
+        print("Correct!")
+
+
+def get_rand_word(lxcn):
+    rand_index = random.randint(0, len(lxcn) - 1)
+    rand_wrd = lxcn[rand_index]
+    return [rand_wrd, rand_index]
+
+
+def get_lex(filepath):
+    with open(filepath, "r") as file:
+        content = file.read()
+    data = json.loads(content)
+    return data
---
+++ main.py
@@ -0,0 +1,75 @@
+import function
+
+while True:
+    user_action = input("add, quiz, delete(del) or show: ")
+    if user_action == "add":
+        while True:
+            option = input("noun, verb, sv, other, back: ")
+            if option == "b":
+                break
+            else:
+                function.add_new_word(option)
+
+    elif user_action == "quiz":
+        while True:
+            option = input("Standart dictionary(sd), Strong verbs(sv), back: ")
+            if option == "sd":
+                lexicon = function.get_lex("standart_words.json")
+                while True:
+                    [wrd, random_index] = function.get_rand_word(lexicon)
+                    if wrd["form"] == "n":
+                        user_answer_ingerman = input(f"{wrd['in_georgian']}: ")
+                        user_answer_inplural = input("in plural: ")
+                        if user_answer_ingerman != wrd["in_german"] or user_answer_inplural != wrd["in_german_plural"]:
+                            print(f"Incorrect, your answer: {user_answer_ingerman} - {user_answer_inplural}  / "
+                                  f"correct: {random_index + 1}.{wrd["in_german"]} - {wrd['in_german_plural']}")
+                        else:
+                            print("Correct!")
+                    elif wrd["form"] == "v":
+                        function.quiz_print(wrd, random_index)
+                    elif wrd["form"] == "o":
+                        function.quiz_print(wrd, random_index)
+            elif option == "sv":
+                lexicon = function.get_lex("sv_dictionary.json")
+                [wrd, random_index] = function.get_rand_word(lexicon)
+                for verb in lexicon:
+                    user_infintive = input(f"{wrd['in_georgian']} Infinitive: ")
+                    user_imperfekt = input("Imperfekt: ")
+                    user_partizip2 = input("Partizip II: ")
+                    user_hilfsverb = input("Hilfsverb: ")
+                    if (wrd["infinitiv"] != user_infintive or wrd["imperfekt"] != user_imperfekt or
+                            wrd["partizip_2"] != user_partizip2 or wrd["hilfsverb"] != user_hilfsverb):
+                        print(
+                            f"Incorrect!, your answer: {user_infintive} - {user_imperfekt} - "
+                            f"{user_partizip2} - {user_hilfsverb}  /"
+                            f"correct: {random_index + 1}.{wrd['infinitiv']} - {wrd['imperfekt']} - "
+                            f"{wrd['partizip_2']} - {wrd['hilfsverb']}")
+                    else:
+                        print("Correct!")
+            elif option == "b":
+                break
+    elif user_action == "del":
+        while True:
+            user_option = input("Standart dictionary(sd), Strong verbs(sv) or back: ")
+            if user_option == "sd" or user_option == "sv":
+                function.del_word(user_option)
+            elif user_option == "b":
+                break
+    elif user_action == "show":
+        user_option = input("Standart dictionary(sd) or Strong verbs(sv): ")
+        if user_option == "sd":
+            lexicon = function.get_lex("standart_words.json")
+            print(len(lexicon))
+            for i, w in enumerate(lexicon):
+                if w["form"] == "n":
+                    print(f"{i + 1}.{w['in_german']} - {w['in_german_plural']} - {w['in_georgian']}")
+                elif w["form"] == "v":
+                    print(f"{i + 1}.{w['in_german']} - {w['in_georgian']}")
+                elif w["form"] == "o":
+                    print(f"{i + 1}.{w['in_german']} - {w['in_georgian']}")
+
+        elif user_option == "sv":
+            lexicon = function.get_lex("sv_dictionary.json")
+            for i, v in enumerate(lexicon):
+                print(f"{i + 1}.{v['infinitiv']} - {v['imperfekt']} - {v['partizip_2']} "
+                      f"- {v['hilfsverb']} - {v['in_georgian']}")
